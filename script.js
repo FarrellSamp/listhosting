44e78d435da517ff5e553a1560fe42145f199808
DitzzJs <yt.adityajr@gmail.com>
2025-11-14 00:58:29 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".cta button");
+  button.addEventListener("click", () => {
+    alert("Welcome to Nerver Indo Roleplay! Redirecting soon...");
+    // window.location.href = "https://discord.gg/your-invite";
+  });
+});
